2e033dcebfcd9be865dcc8ebd67bdd2b7a55eeaf
whyAsif <unknownboy398@gmail.com>
2024-03-08 01:55:51 +0600

    First Commit

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
@@ -8,19 +9,24 @@ import net.dv8tion.jda.api.sharding.ShardManager;
 import javax.security.auth.login.LoginException;
 
 public class Bot {
-
+    private final Dotenv config;
     private final ShardManager shardManager;
 
     public Bot() throws LoginException{
-
-        String token="MTIxNTMwNzIzMzIxNTI1NDU3MA.GIUg6E.tBtVFs0lHACoIjNqjotiyohjsYqYQSQo-ALFps";
+        config= Dotenv.configure().load();
+        String token = config.get("TOKEN");
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.playing("Minecraft"));
         shardManager=builder.build();
 
     }
-    public ShardManager getShardManager() { return shardManager; }
+    public Dotenv getConfig(){
+        return config;
+    }
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
 
     public static void main(String[] args) {
         try {
